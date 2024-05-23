3972eebb6ffd908425e765f6b35779369bb1ec2b
Gyulari <gyulari7160@gmail.com>
2024-05-23 18:13:34 +0900

    바둑판에 바둑알 두는 기능 구현

--- SuperMoku/Assets/Scripts/GoBoardManager.cs
+++ SuperMoku/Assets/Scripts/GoBoardManager.cs
@@ -24,16 +24,49 @@ public class GoBoardManager : MonoBehaviour
 {
     Vector3 mousePos;
 
+    GoBoard goBoard;
+
+    public List<GameObject> stones;
+
+    GameObject cursorStone;
+
+    int count = 0;
+
     void Start()
     {
-        GoBoard goBoard = new GoBoard(new StoneType[15, 15]);
+        goBoard = new GoBoard(new StoneType[15, 15]);
+        cursorStone = Instantiate(stones[2], transform);
     }
 
     void Update()
     {
         (int x, int y) gridIndex = MousePosToGridIndex(Input.mousePosition);
 
-        Debug.Log("[" + gridIndex.x + "][" + gridIndex.y + "]");
+        if(gridIndex.x >= 0 && gridIndex.y >= 0 && gridIndex.x < 15 && gridIndex.y < 15){
+            if (goBoard.m_Grid[gridIndex.x, gridIndex.y] == StoneType.Empty) {
+                // goBoard.m_Grid[gridIndex.x, gridIndex.y] = StoneType.Black;
+                cursorStone.SetActive(true);
+                cursorStone.transform.position = new Vector3((gridIndex.x - 7) / 15f * 10f, 0.05f, (gridIndex.y - 7) / 15f * 10f);
+                // stone = Instantiate(stones[(int)StoneType.Black - 1], new Vector3((gridIndex.x - 7) / 15f * 10f, 0.05f, (gridIndex.y - 7) / 15f * 10f), Quaternion.identity);
+                // cursorStone.transform.parent = gameObject.transform;
+                // Debug.Log("[" + gridIndex.x + "][" + gridIndex.y + "]");
+            }
+        }
+        else {
+            cursorStone.SetActive(false);
+        }
+
+        if (Input.GetMouseButtonDown(0) && goBoard.m_Grid[gridIndex.x, gridIndex.y] == StoneType.Empty) {
+            if (count % 2 == 0) {
+                goBoard.m_Grid[gridIndex.x, gridIndex.y] = StoneType.Black;
+                GameObject stone = Instantiate(stones[(int)StoneType.Black - 1], new Vector3((gridIndex.x - 7) / 15f * 10f, 0.05f, (gridIndex.y - 7) / 15f * 10f), Quaternion.identity);
+            }
+            else {
+                goBoard.m_Grid[gridIndex.x, gridIndex.y] = StoneType.White;
+                GameObject stone = Instantiate(stones[(int)StoneType.White - 1], new Vector3((gridIndex.x - 7) / 15f * 10f, 0.05f, (gridIndex.y - 7) / 15f * 10f), Quaternion.identity);
+            }
+            count++;
+        }
     }
 
     private (int xIdx, int yIdx) MousePosToGridIndex(Vector3 mousePos)
