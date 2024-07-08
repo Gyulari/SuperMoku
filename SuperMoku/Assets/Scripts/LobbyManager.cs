db35b6db08cea7388ef5a9b60bbf29992ac1f8df
Gyulari <gyulari7160@gmail.com>
2024-07-08 17:59:20 +0900

    로비 생성 및 나가기 구현

--- SuperMoku/Assets/Scripts/LobbyManager.cs
+++ SuperMoku/Assets/Scripts/LobbyManager.cs
@@ -5,6 +5,9 @@ using Steamworks;
 using Steamworks.Data;
 using UnityEngine.Events;
 using UnityEngine.UI;
+using System.Threading.Tasks;
+using Unity.VisualScripting;
+using TMPro;
 
 public class LobbyManager : MonoBehaviour
 {
@@ -23,7 +26,19 @@ public class LobbyManager : MonoBehaviour
     {
         DontDestroyOnLoad(this);
 
+        SteamMatchmaking.OnLobbyCreated += OnLobbyCreatedCallBack;
+        SteamMatchmaking.OnLobbyEntered += OnLobbyEntered;
+        SteamMatchmaking.OnLobbyMemberJoined += OnLobbyMemberJoined;
+        SteamMatchmaking.OnChatMessage += OnChatMessage;
+        SteamMatchmaking.OnLobbyMemberDisconnected += OnLobbyMemberDisconnected;
+        SteamMatchmaking.OnLobbyGameCreated += OnLobbyGameCreated;
+        SteamFriends.OnGameLobbyJoinRequested += OnGameLobbyJoinRequest;
+        SteamMatchmaking.OnLobbyInvite += OnLobbyInvite;
+    }
 
+    void OnLobbyInvite(Friend friend, Lobby lobby)
+    {
+        Debug.Log($"{friend.Name} invited you to his lobby.");
     }
 
     private void OnLobbyGameCreated(Lobby lobby, uint ip, ushort port, SteamId id)
@@ -36,6 +51,116 @@ public class LobbyManager : MonoBehaviour
         Debug.Log($"{friend.Name} joined the lobby");
         GameObject obj = Instantiate(InLobbyFriend, content);
         obj.GetComponentInChildren<Text>().text = friend.Name;
-        // obj.GetComponentInChildren<RawImage>().texture = await SteamFriendsManager.GetTextureFromSteamIdAsync(friend.Id);
+        obj.GetComponentInChildren<RawImage>().texture = await SteamFriendsManager.GetTextureFromSteamIdAsync(friend.Id);
+        inLobby.Add(friend.Id, obj);
+    }
+
+    void OnLobbyMemberDisconnected(Lobby lobby, Friend friend)
+    {
+        Debug.Log($"{friend.Name} left the lobby");
+        Debug.Log($"New lobby owner is {currentLobby.Owner}");
+        if (inLobby.ContainsKey(friend.Id)) {
+            Destroy(inLobby[friend.Id]);
+            inLobby.Remove(friend.Id);
+        }
+    }
+
+    void OnChatMessage(Lobby lobby, Friend friend, string message)
+    {
+        Debug.Log($"incoming chat message from {friend.Name} : {message}");
+    }
+
+    async void OnGameLobbyJoinRequest(Lobby joinedLobby, SteamId id)
+    {
+        RoomEnter joinedLobbySuccess = await joinedLobby.Join();
+        if(joinedLobbySuccess != RoomEnter.Success) {
+            Debug.Log("failed to join lobby : " + joinedLobbySuccess);
+        }
+        else {
+            currentLobby = joinedLobby;
+        }
+    }
+
+    void OnLobbyCreatedCallBack(Result result, Lobby lobby)
+    {
+        if(result != Result.OK) {
+            Debug.Log("lobby creation result not ok : " + result);
+        }
+        else {
+            OnLobbyCreated.Invoke();
+            Debug.Log("lobby creation result ok");
+        }
+    }
+
+    async void OnLobbyEntered(Lobby lobby)
+    {
+        Debug.Log("Client joined the lobby");
+
+        foreach(var user in inLobby.Values) {
+            Destroy(user);
+        }
+        inLobby.Clear();
+
+        GameObject obj = Instantiate(InLobbyFriend, content);
+        obj.GetComponentInChildren<TMP_Text>().text = SteamClient.Name;
+        obj.GetComponentInChildren<RawImage>().texture = await SteamFriendsManager.GetTextureFromSteamIdAsync(SteamClient.SteamId);
+
+        inLobby.Add(SteamClient.SteamId, obj);
+
+        foreach(var friend in currentLobby.Members) {
+            if(friend.Id != SteamClient.SteamId) {
+                GameObject obj2 = Instantiate(InLobbyFriend, content);
+                obj2.GetComponentInChildren<Text>().text = friend.Name;
+                obj2.GetComponentInChildren<RawImage>().texture = await SteamFriendsManager.GetTextureFromSteamIdAsync(friend.Id);
+
+                inLobby.Add(friend.Id, obj2);
+            }
+        }
+        OnLobbyJoined.Invoke();
+    }
+
+    public async void CreateLobbyAsync()
+    {
+        bool result = await CreateLobby();
+        if (!result) {
+            // Invoke a error message.
+        }
+    }
+
+    public async Task<bool> CreateLobby()
+    {
+        try {
+            var createLobbyOutput = await SteamMatchmaking.CreateLobbyAsync();
+            if (!createLobbyOutput.HasValue) {
+                Debug.Log("Lobby created but not correctly instantiated.");
+                return false;
+            }
+            currentLobby = createLobbyOutput.Value;
+
+            currentLobby.SetPublic();
+            // currentLobby.SetPrivate();
+            currentLobby.SetJoinable(true);
+
+            return true;
+        }
+        catch(System.Exception exception) {
+            Debug.Log("Failed to create multiplayer lobby : " + exception.ToString());
+            return false;
+        }
+    }
+
+    public void LeaveLobby()
+    {
+        try {
+            currentLobby.Leave();
+            OnLobbyLeave.Invoke();
+            foreach(var user in inLobby.Values) {
+                Destroy(user);
+            }
+            inLobby.Clear();
+        }
+        catch {
+
+        }
     }
 }
--- SuperMoku/Assets/Scripts/SteamFriendsManager.cs
+++ SuperMoku/Assets/Scripts/SteamFriendsManager.cs
@@ -2,6 +2,7 @@ using Steamworks;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using Steamworks.ServerList;
 
 public class SteamFriendsManager : MonoBehaviour
 {
@@ -43,9 +44,30 @@ public class SteamFriendsManager : MonoBehaviour
                 $"{friend.Name}({friend.Id}) | online : {friend.IsOnline} | playing this game : {friend.IsPlayingThisGame}"
                 );
             GameObject f = Instantiate(friendObj, friendsContent);
-            var img = await SteamFriends.GetLargeAvatarAsync(friend.Id);
-            f.GetComponentInChildren<RawImage>().texture = GetTextureFromImage(img.Value);
             f.GetComponentInChildren<TMP_Text>().text = friend.Name;
+            AssignFriendImage(f, friend.Id);
         }
+    }
+
+    public async void AssignFriendImage(GameObject f, SteamId id)
+    {
+        var img = await SteamFriends.GetLargeAvatarAsync(id);
+        f.GetComponentInChildren<RawImage>().texture = GetTextureFromImage(img.Value);
+    }
+
+    public static async System.Threading.Tasks.Task<Texture2D> GetTextureFromSteamIdAsync(SteamId Id)
+    {
+        var img = await SteamFriends.GetLargeAvatarAsync(SteamClient.SteamId);
+        Steamworks.Data.Image image = img.Value;
+        Texture2D texture = new Texture2D((int)image.Width, (int)image.Height);
+
+        for(int x=0; x<image.Width; x++) {
+            for(int y=0; y<image.Height; y++) {
+                var p = image.GetPixel(x, y);
+                texture.SetPixel(x, (int)image.Height - y, new Color(p.r / 255.0f, p.g / 255.0f, p.b / 255.0f, p.a / 255.0f));
+            }
+        }
+        texture.Apply();
+        return texture;
     }
 }
